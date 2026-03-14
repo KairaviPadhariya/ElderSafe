6e420a51a6b5b9993ea9c51ac8377cd0fd0754c6
Chandni-K12 <kotharichandni183@gmail.com>
2026-03-14 16:29:55 +0530

    Resolved merge conflicts

--- app/routes/notifications.py
+++ app/routes/notifications.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter, Depends
-<<<<<<< HEAD
-=======
-from app.utils.auth import verify_token
->>>>>>> ed18f6d4982d3b037207c70cce893526eb36eeaf
 from app.database import database
 from app.schemas.notification import NotificationCreate
 from app.utils.auth import verify_token
+from datetime import datetime
 
 router = APIRouter()
 
@@ -16,15 +13,13 @@ async def create_notification(
 ):
     notification_dict = notification.dict()
     notification_dict["created_at"] = datetime.utcnow()
+
     result = await database.notifications.insert_one(notification_dict)
+
     return {"notification_id": str(result.inserted_id)}
 
-<<<<<<< HEAD
-router.get("/notifications")
-=======
 
 @router.get("/notifications")
->>>>>>> ed18f6d4982d3b037207c70cce893526eb36eeaf
 async def get_notifications(current_user: str = Depends(verify_token)):
     notifications = []
     async for n in database.notifications.find():
